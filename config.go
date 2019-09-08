9c24a5d6136b1d491840bca8a6e66ca1af724e72
amirrezaask <raskarpour@gmail.com>
2019-09-08 17:11:08 +0430

    Revert "new structure for SOA projects"
    This reverts commit 818dd7b669a837cfefa7a54b440bb9b39c615377.

--- config.go
+++ config.go
@@ -2,7 +2,6 @@ package athena
 
 import (
 	"fmt"
-	"io"
 	"os"
 	"strings"
 
@@ -10,44 +9,52 @@ import (
 	"github.com/pkg/errors"
 )
 
-//Cfg is a placeholder for config
-type Cfg struct {
-	m map[string]string
-}
+//Get is a proxy to C().Get
+var Get func(key string) string
 
-func (c *Cfg) Get(key string) string {
-	val, exists := (c.m)[key]
+//Map represnets config
+type Map map[string]string
+
+//Get gets a key from config map
+func (c Map) Get(key string) string {
+	val, exists := (c)[key]
 	if !exists {
 		return ""
 	}
 	return val
 }
-func (c *Cfg) Set(key string, val string) {
-	c.m[key] = val
+
+//Set sets a key config
+func (c *Map) Set(key, value string) {
+	(*c)[key] = value
+}
+
+var config *Map
+
+//C gets the global config object
+func C() Map {
+	return *config
 }
 
 //PrettyPrint prints pretty
-func (c *Cfg) PrettyPrint() string {
+func (c Map) PrettyPrint() string {
 	output := "Key => Value\n"
-	for k, v := range c.m {
+	for k, v := range c {
 		output += fmt.Sprintf("'%s' = '%s'\n", k, v)
 	}
 	return output
 }
 
 //Init initialize config
-func Init(r io.Reader) *Cfg {
-	dotEnv, err := godotenv.Parse(r)
+func Init(envsToRead []string) {
+	err := godotenv.Load()
 	if err != nil {
 		fmt.Fprint(os.Stderr, errors.Wrap(err, "error in loading env file").Error())
 	}
-	for k, _ := range dotEnv {
-		env := os.Getenv(strings.ToUpper(k))
-		if env != "" {
-			dotEnv[k] = env
-		}
+	config = &Map{}
+	for _, e := range envsToRead {
+		v := os.Getenv(strings.ToUpper(e))
+		config.Set(strings.ToLower(e), v)
 	}
-	return &Cfg{
-		dotEnv,
-	}
+	Get = config.Get
 }
